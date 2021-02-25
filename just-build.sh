6c1ebbac77e5364659577ac5e1add0135498e634
Ron Olson <tachoknight@gmail.com>
2021-02-25 12:35:48 -0600

    Changes for Fedora 35

--- just-build.sh
+++ just-build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# If necessary:
+dnf install -y fedora-packager fedora-review
 
-rm -rf /home/rolson/rpmbuild
-mkdir -p /home/rolson/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cp $PWD/*.patch /home/rolson/rpmbuild/SOURCES
-cp $PWD/nethack.desktop /home/rolson/rpmbuild/SOURCES
-cp $PWD/nethack.spec /home/rolson/rpmbuild/SPECS
+rm -rf $HOME/rpmbuild
+mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp $PWD/*.patch $HOME/rpmbuild/SOURCES
+cp $PWD/nethack.desktop $HOME/rpmbuild/SOURCES
+cp $PWD/nethack.spec $HOME/rpmbuild/SPECS
 
-pushd /home/rolson/rpmbuild/SPECS
+pushd $HOME/rpmbuild/SPECS
 spectool -g -R ./nethack.spec
 # Get the dependencies
 dnf builddep -y ./nethack.spec
