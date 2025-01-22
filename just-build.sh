284354d048a0c49acc042dcd3ad34c903a736ad6
Ron Olson <tachoknight@gmail.com>
2025-01-22 15:37:34 -0600

    updated build script

--- just-build.sh
+++ just-build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+SECONDS=0
+
+echo Building Everything for Fedora `rpm -E %fedora` | figlet -c -f mini | lolcat
+
+MYDIR=$PWD
+
+START_TS=`date`
+
 # If necessary:
 dnf install -y fedora-packager fedora-review
 
@@ -12,8 +20,18 @@ cp $PWD/nethack.spec $HOME/rpmbuild/SPECS
 pushd $HOME/rpmbuild/SPECS
 spectool -g -R ./nethack.spec
 # Get the dependencies
-dnf builddep -y ./nethack.spec
+dnf builddep -y ./nethack.spec 2>&1 | tee $MYDIR/build-output.txt
 # Now do the actual build
 rpmbuild -ba ./nethack.spec
 popd
 
+echo Started:_____$START_TS
+echo Ended:_______`date`
+
+# Now tell us how long it took
+hours=$((SECONDS / 3600))
+minutes=$(( (SECONDS % 3600) / 60 ))
+seconds=$((SECONDS % 60))
+
+# Print the elapsed time
+echo "Elapsed Time: $hours hour(s) $minutes minute(s) $seconds second(s)"
